243fc7ffa471d4fa6abf5e2b4444d3a41fd62acb
Han Lin <HanLin.Shang@gmail.com>
2019-12-08 16:54:08 +1100

    Add files via upload

--- daily_North_Dakota_weather_data.R
+++ daily_North_Dakota_weather_data.R
@@ -1,5 +1,5 @@
 rm(list=ls())
-source("Required_functions.R")
+source("auxiliary_functions.R")
 
 load("temperature_daily.Rda")
 load("wind_daily.Rda")
@@ -50,73 +50,95 @@ train_stations = c(1:50) #training stations
 test_stations = c(51:70) #test stations
 
 ###Inner product matrices
-Inn_prod = vector("list",)
+m_basis_y = create.bspline.basis(rangeval = range(d_time), nbasis = smoothing_params_Y[1], norder=4)
+Inn_prod_y = inprod(m_basis_y, m_basis_y)
+
+Inn_prod_x = vector("list",)
 for(i in 1:np){
-  m_basis = create.bspline.basis(rangeval = range(d_time), nbasis = smoothing_params_X[[i]][1], norder=4)
-  Inn_prod[[i]] = inprod(m_basis, m_basis)
+  m_basis_x = create.bspline.basis(rangeval = range(d_time), nbasis = smoothing_params_X[[i]][1], norder=4)
+  Inn_prod_x[[i]] = inprod(m_basis_x, m_basis_x)
 }
+
+###Square roots of the inner product matrices
+Inn_prod_y_sqrt = sqrtm(Inn_prod_y)
+Inn_prod_x_sqrt = vector("list",)
+
+for(i in 1:np)
+  Inn_prod_x_sqrt[[i]] = sqrtm(Inn_prod_x[[i]])
+
+###Arguments for the PLS regression
+#Response
+Reg_y = Reg_y_out = S_Y %*% Inn_prod_y_sqrt
+
+#Predictors
+Reg_mat = vector("list",)
+
+for(i in 1:np){
+  Reg_mat[[i]] = S_X[[i]] %*% Inn_prod_x_sqrt[[i]]
+}
+
+###Divide the data into training and test samples
+#Response
+Reg_y_train = Reg_y[train_stations,]
+
+#Predictors
+Reg_mat_train = vector("list",)
+Reg_mat_test = vector("list",)
+
+for(i in 1:np){
+  Reg_mat_train[[i]] = Reg_mat[[i]][train_stations,]
+  Reg_mat_test[[i]] = Reg_mat[[i]][test_stations,]
+}
+
+Reg_mat_train = do.call(cbind, Reg_mat_train)
+Reg_mat_test = do.call(cbind, Reg_mat_test)
+
+###Mean of variables
+mean_y = apply(Reg_y_train, 2, mean)
+
+mean_x = apply(Reg_mat_train, 2, mean)
+mean_x_test = apply(Reg_mat_test, 2, mean)
 
 
 ###Center the response and predictors for training stations
 #Response
-CY = matrix(NA, nrow = length(train_stations), ncol = ncol(S_Y))
-for(i in 1:length(train_stations))
-  CY[i,] = S_Y[train_stations,][i,] - apply(S_Y[train_stations,], 2, mean)
+CY = matrix(NA, nrow = nrow(Reg_y_train), ncol = ncol(Reg_y_train))
+for(i in 1:nrow(Reg_y_train))
+  CY[i,] = Reg_y_train[i,] - mean_y
 
 #Predictors
-CX = vector("list",)
-for(j in 1:np){
-  cen_x = matrix(NA, nrow = length(train_stations), ncol = ncol(S_X[[j]]))
-  for(i in 1:length(train_stations))
-    cen_x[i,] = S_X[[j]][train_stations,][i,] - apply(S_X[[j]][train_stations,], 2, mean)
-  CX[[j]] = cen_x
-}
+CX = matrix(NA, nrow = nrow(Reg_mat_train), ncol = ncol(Reg_mat_train))
+for(i in 1:nrow(Reg_mat_train))
+  CX[i,] = Reg_mat_train[i,] - mean_x
 
 ###Center the predictors for test stations
-CX_test = vector("list",)
-for(j in 1:np){
-  cen_x_test = matrix(NA, nrow = length(test_stations), ncol = ncol(S_X[[j]]))
-  for(i in 1:length(test_stations))
-    cen_x_test[i,] = S_X[[j]][test_stations,][i,] - apply(S_X[[j]][test_stations,], 2, mean)
-  CX_test[[j]] = cen_x_test
-}
-
-###Z matrix for the regression
-#Training sample
-Z = NULL
-for(i in 1:np){
-  Z = cbind(Z, CX[[i]] %*% Inn_prod[[i]])
-}
-
-#Test sample
-Z_test = NULL
-for(i in 1:np){
-  Z_test = cbind(Z_test, CX_test[[i]] %*% Inn_prod[[i]])
-}
+CX_test = matrix(NA, nrow = nrow(Reg_mat_test), ncol = ncol(Reg_mat_test))
+for(i in 1:nrow(Reg_mat_test))
+  CX_test[i,] = Reg_mat_test[i,] - mean_x_test
 
 ###SIMPLS
-model_simpls = pls.regression(Xtrain = Z, Ytrain = CY, Xtest = Z,
+model_simpls = pls.regression(Xtrain = CX, Ytrain = CY, Xtest = CX,
                             ncomp = 5, unit.weights=FALSE)
 Bhat_simpls = model_simpls$B
 
 
 ###NIPALS
-model_nipals = plsreg2(predictors = Z, responses = CY, comps = 5,crosval = TRUE)
+model_nipals = plsreg2(predictors = CX, responses = CY, comps = 5,crosval = TRUE)
 Bhat_nipals = model_nipals$reg.coefs[1:(smoothing_params_X1[1]+smoothing_params_X2[1]),]
 
 
 ###Obtain fitted coefficients
-fitted_coefs_simpls = Z_test%*%Bhat_simpls
-fitted_coefs_nipals = Z_test%*%Bhat_nipals
+fitted_coefs_simpls = CX_test%*%Bhat_simpls
+fitted_coefs_nipals = CX_test%*%Bhat_nipals
 
 for(i in 1: length(test_stations)){
-  fitted_coefs_simpls[i,] = fitted_coefs_simpls[i,] + apply(S_Y[train_stations,], 2, mean)
-  fitted_coefs_nipals[i,] = fitted_coefs_nipals[i,] + apply(S_Y[train_stations,], 2, mean)
+  fitted_coefs_simpls[i,] = fitted_coefs_simpls[i,] + mean_y
+  fitted_coefs_nipals[i,] = fitted_coefs_nipals[i,] + mean_y
 }
 
 ###Obtain fitted functions
-fitted_functions_simpls = fitted_coefs_simpls%*%t(Phi_y)
-fitted_functions_nipals = fitted_coefs_nipals%*%t(Phi_y)
+fitted_functions_simpls = (fitted_coefs_simpls %*% solve(Inn_prod_y_sqrt))%*%t(Phi_y)
+fitted_functions_nipals = (fitted_coefs_nipals %*% solve(Inn_prod_y_sqrt))%*%t(Phi_y)
 
 ts.plot(t(solar_fun[test_stations,])) # Time series plot of the observed solar radiation functions in the test sample
 ts.plot(t(fitted_functions_simpls)) # Time series plot of the fitted solar radiation functions in the test sample (SIMPLS)
